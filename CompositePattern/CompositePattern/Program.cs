7f6c194df9fad65adfa92b6910e9c94f64c7508e
Ohishi Kenji <k.divocmkva.147@gmail.com>
2017-12-05 11:30:50 +0900

    try-catchを削除

--- CompositePattern/CompositePattern/Program.cs
+++ CompositePattern/CompositePattern/Program.cs
@@ -10,37 +10,31 @@ namespace CompositePattern
     {
         static void Main(string[] args)
         {
-            try
-            {
-                var root = new Folder("root");
-                var user = new Folder("user");
-                var temp = new Folder("temp");
-                var documents = new Folder("document");
-                var game = new Folder("げぇむ");
+            var root = new Folder("root");
+            var user = new Folder("user");
+            var temp = new Folder("temp");
+            var documents = new Folder("document");
+            var game = new Folder("げぇむ");
 
-                var gomi = new File("ごみ");
-                var ika = new File("ika");
-                var tako = new File("tako");
-                var salmon = new File("salmon");
+            var gomi = new File("ごみ");
+            var ika = new File("ika");
+            var tako = new File("tako");
+            var salmon = new File("salmon");
 
-                //フォルダを追加
-                root.AddEntry(user);
-                root.AddEntry(temp);
-                user.AddEntry(documents);
-                user.AddEntry(game);
+            //フォルダを追加
+            root.AddEntry(user);
+            root.AddEntry(temp);
+            user.AddEntry(documents);
+            user.AddEntry(game);
 
-                //ファイルを追加
-                temp.AddEntry(gomi);
-                game.AddEntry(ika);
-                game.AddEntry(tako);
-                game.AddEntry(salmon);
+            //ファイルを追加
+            temp.AddEntry(gomi);
+            game.AddEntry(ika);
+            game.AddEntry(tako);
+            game.AddEntry(salmon);
 
-                root.Output(0);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Exceptionが発生しました");
-            }
+            root.Output(0);
+
         }
     }
 }
